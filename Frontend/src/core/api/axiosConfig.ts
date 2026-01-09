9a507f686b05b63556fb54df0a529681f54a8e67
Kenneth Cacho <khentplays@gmail.com>
2026-01-09 20:56:51 +0800

    fix(frontend): add prefix on axios config for api url

--- Frontend/src/core/api/axiosConfig.ts
+++ Frontend/src/core/api/axiosConfig.ts
@@ -3,7 +3,7 @@ import { LoadEnv } from '../config/load.env'
 
 const API_URL_KEY = import.meta.env.PROD ? "VITE_PROD_API_URL" : "VITE_DEV_API_URL"
 export const axiosInstance: AxiosInstance = axios.create({
-    baseURL: LoadEnv(API_URL_KEY),
+    baseURL: `https://${LoadEnv(API_URL_KEY)}`,
     timeout: 30000,
     withCredentials: true,
     headers: {
