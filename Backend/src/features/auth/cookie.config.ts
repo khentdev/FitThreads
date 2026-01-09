f708932e68923623fcaa10e02c81068939d3db90
Kenneth Cacho <khentplays@gmail.com>
2026-01-09 22:05:21 +0800

    fix: rollback to previous cookie config and update because I accidentaly removed the some part.

--- Backend/src/features/auth/cookie.config.ts
+++ Backend/src/features/auth/cookie.config.ts
@@ -1,7 +1,7 @@
 import { Context } from 'hono';
-import { setCookie, getCookie, deleteCookie } from 'hono/cookie';
-import { env } from "../../configs/env.js"
+import { deleteCookie, getCookie, setCookie } from 'hono/cookie';
 import type { CookieOptions } from "hono/utils/cookie";
+import { env } from "../../configs/env.js";
 
 type CookieOptionsParams = {
     context: "auth" | "csrf"
@@ -10,6 +10,34 @@ type CookieOptionsParams = {
 
 type CookieNames = "sid" | "csrfToken"
 type CookieParams = {
+    c: Context;
+    name: CookieNames;
+    value: string;
+    options: Pick<CookieOptionsParams, "maxAge">;
+};
+
+const createCookieOptions = ({ context, maxAge }: CookieOptionsParams): CookieOptions => {
+    const isProd = env.NODE_ENV === "production"
+    return {
+        domain: ".fitthreads.blog",
+        path: "/",
+        secure: isProd,
+        httpOnly: context === "auth",
+        sameSite: "lax",
+        maxAge,
+        prefix: isProd ? "secure" : undefined,
+    }
+}
+
+export const setAuthCookie = ({ c, name, value, options }: CookieParams) =>
+    setCookie(c, name, value, createCookieOptions({ context: "auth", ...options }))
+export const setCSRFCookie = ({ c, name, value, options }: CookieParams) =>
+    setCookie(c, name, value, createCookieOptions({ context: "csrf", ...options }))
 
 export const getCookieValue = (c: Context, name: any) => getCookie(c, name);
-export const deleteAuthCookie = (c: Context, name: any) => deleteCookie(c, name, { path: "/", secure: env.NODE_ENV === "production", sameSite: env.NODE_ENV === "production" ? "none" : "lax" });
+export const deleteAuthCookie = (c: Context, name: any) => deleteCookie(c, name, {
+  path: "/",
+  domain: ".fitthreads.blog",
+  secure: env.NODE_ENV === "production",
+  sameSite: "lax",
+})
